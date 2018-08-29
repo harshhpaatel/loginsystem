86342f82ecd0f734b339b9e5a0a8dae8b5192769
harshhpaatel <harshamit10@gmail.com>
2018-08-29 16:30:31 -0400

    Initial Commit

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@
 $host="localhost";
 $user="db_user";
 $password="db_password";
-$db="id6882724_livestream";
+$db="db_db";
 
 $con = mysqli_connect($host,$user,$password);
 mysqli_select_db($con, $db) or die(mysqli_error($con));
