8d57da5972cf34dde038bf938820523f5bedce64
harshhpaatel <harshamit10@gmail.com>
2018-12-17 19:57:03 -0500

    Initial Commit

--- onelevelsystem/connection.php
+++ onelevelsystem/connection.php
@@ -1,9 +1,9 @@
 <?php
 
 $host="localhost";
-$user="id6882724_dradh";
-$password="bapa2133";
-$db="id6882724_livestream";
+$user="db_user";
+$password="db_password";
+$db="db_db";
 
 $con = mysqli_connect($host,$user,$password);
 mysqli_select_db($con, $db) or die(mysqli_error($con));
